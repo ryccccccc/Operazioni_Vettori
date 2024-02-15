8e1fb4dcfcf59957582fd35557cf585b5e396f6e
Riccardo Nervosi <riccardo.nervosi@gmail.com>
2024-02-15 23:04:46 +0100

    Eliminato metodo Somma

---
+++ Operazioni_Vettori/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Operazioni_Vettori
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(255, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(255, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(266, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(255, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(342, 314);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "RISULTATO";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(322, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 46);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "scalare * vettore";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(322, 234);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 46);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "vettore * scalare";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(448, 182);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(73, 46);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "X (prod. vettoriale)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(416, 297);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 46);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "+/-";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(448, 237);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(73, 43);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "/";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(536, 67);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(87, 46);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "VERSORE";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(536, 119);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(87, 46);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "VERSORE";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(629, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "RISULTATO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(629, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "RISULTATO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 519);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Operazioni_Vettori/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Operazioni_Vettori
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Vector v1 = Vector.Parse(textBox1.Text);
+            Vector v2 = Vector.Parse(textBox2.Text);
+            label1.Text = (v1 + v2).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Vector v1 = Vector.Parse(textBox1.Text);
+            Vector v2 = Vector.Parse(textBox2.Text);
+            label1.Text = (v1 - v2).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /* if (Vector.TryParse(Console.ReadLine(), ref v1))
+           {
+               Console.WriteLine(v1 + v2);
+           }
+           else
+           {
+               Console.WriteLine("COMPONENTI NON CORRETTE");
+           }
+           if (Vector.TryParse(Console.ReadLine(), ref v2))
+           {
+               Console.WriteLine(v1 + v2);
+           }
+           else
+           {
+               Console.WriteLine("COMPONENTI NON CORRETTE");
+           }*/
+            int n = int.Parse(textBox1.Text);
+            Vector v = Vector.Parse(textBox2.Text);
+            label1.Text = (n * v).ToString();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Vector v = Vector.Parse(textBox1.Text);
+            int n = int.Parse(textBox2.Text);
+            label1.Text = (v * n).ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Vector v1 = Vector.Parse(textBox1.Text);
+            Vector v2 = Vector.Parse(textBox2.Text);
+            label1.Text = (v1 * v2).ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Vector v = Vector.Parse(label1.Text);
+            label1.Text = (-v).ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Vector v1 = Vector.Parse(textBox1.Text);
+            Vector v2 = Vector.Parse(textBox2.Text);
+            label1.Text = (v1 / v2).ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Vector v = Vector.Parse(textBox1.Text);
+            label2.Text = (v.Versore()).ToString();
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Vector v = Vector.Parse(textBox2.Text);
+            label3.Text = (v.Versore()).ToString();
+        }
+    }
+}
